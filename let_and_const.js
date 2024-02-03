8b2d8e6e9afcf759fa76c55f6a78884f89f835cf
Domenick <152310637+ddddsfsgf@users.noreply.github.com>
2024-02-03 13:43:51 -0500

    Add files via upload

---
+++ let_and_const.js
@@ -0,0 +1,10 @@
+//ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+//Es2015 Globsl Constants
+const PI = 3.14;
+PI = 42;//TypeError
+
+let PI = 3.14;
+PI = 42;//PI is redeclared
